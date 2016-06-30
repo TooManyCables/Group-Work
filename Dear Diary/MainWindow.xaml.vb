beacd5e75bece07209720a7b244840fa1c0eb57d
Johnstone1 <tyler.johnstone1@DETNSW>
2016-06-30 13:50:39 +1000

    initial commit

---
+++ Dear Diary/MainWindow.xaml.vb
@@ -0,0 +1,17 @@
+﻿Class MainWindow 
+
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+
+        'Sub written by Steven.
+
+        'Make the code, and if there's time, create a transition animation.
+
+        Dim Selc As New Selection
+        Selc.Show()
+        Me.Close()
+
+    End Sub
+
+
+End Class
