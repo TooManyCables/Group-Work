a113e1d05d1cd9a9bedc27ffa10ea7191511a72c
Steven Young <Steven Young>
2016-07-24 16:59:01 +1000

    i forget what i ded

--- Dear Diary/Document.xaml.vb
+++ Dear Diary/Document.xaml.vb
@@ -1,3 +1,14 @@
 ﻿Public Class Document
 
+    Sub New()
+
+        InitializeComponent()
+
+
+
+    End Sub
+
+    Private Sub txtFontSize_LostFocus(sender As Object, e As RoutedEventArgs) Handles txtFontSize.LostFocus
+
+    End Sub
 End Class
